f63d8440fc39839d8987bd0679fa088519e1aff9
Rustam <m-rustam1996@rambler.ru>
2016-07-03 18:42:10 +0300

    Cart. Design basket_item

--- personal/cart/index.php
+++ personal/cart/index.php
@@ -1,34 +1,42 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Корзина");
-?><?$APPLICATION->IncludeComponent("bitrix:sale.basket.basket", "", array(
-	"COUNT_DISCOUNT_4_ALL_QUANTITY" => "N",
-	"COLUMNS_LIST" => array(
-		0 => "NAME",
-		1 => "DISCOUNT",
-		2 => "PRICE",
-		3 => "QUANTITY",
-		4 => "SUM",
-		5 => "PROPS",
-		6 => "DELETE",
-		7 => "DELAY",
-	),
-	"AJAX_MODE" => "N",
-	"AJAX_OPTION_JUMP" => "N",
-	"AJAX_OPTION_STYLE" => "Y",
-	"AJAX_OPTION_HISTORY" => "N",
-	"PATH_TO_ORDER" => "/personal/order/make/",
-	"HIDE_COUPON" => "N",
-	"QUANTITY_FLOAT" => "N",
-	"PRICE_VAT_SHOW_VALUE" => "Y",
-	"TEMPLATE_THEME" => "site",
-	"SET_TITLE" => "Y",
-	"AJAX_OPTION_ADDITIONAL" => "",
-	"OFFERS_PROPS" => array(
-		0 => "SIZES_SHOES",
-		1 => "SIZES_CLOTHES",
-		2 => "COLOR_REF",
-	),
-	),
-	false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:sale.basket.basket",
+	"template1",
+	Array(
+		"ACTION_VARIABLE" => "basketAction",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AUTO_CALCULATION" => "Y",
+		"COLUMNS_LIST" => array("NAME","DELETE","PRICE","QUANTITY","SUM"),
+		"COUNT_DISCOUNT_4_ALL_QUANTITY" => "N",
+		"GIFTS_BLOCK_TITLE" => "Выберите один из подарков",
+		"GIFTS_CONVERT_CURRENCY" => "N",
+		"GIFTS_HIDE_BLOCK_TITLE" => "N",
+		"GIFTS_HIDE_NOT_AVAILABLE" => "N",
+		"GIFTS_MESS_BTN_BUY" => "Выбрать",
+		"GIFTS_MESS_BTN_DETAIL" => "Подробнее",
+		"GIFTS_PAGE_ELEMENT_COUNT" => "4",
+		"GIFTS_PLACE" => "BOTTOM",
+		"GIFTS_PRODUCT_PROPS_VARIABLE" => "prop",
+		"GIFTS_PRODUCT_QUANTITY_VARIABLE" => "",
+		"GIFTS_SHOW_DISCOUNT_PERCENT" => "Y",
+		"GIFTS_SHOW_IMAGE" => "Y",
+		"GIFTS_SHOW_NAME" => "Y",
+		"GIFTS_SHOW_OLD_PRICE" => "N",
+		"GIFTS_TEXT_LABEL_GIFT" => "Подарок",
+		"HIDE_COUPON" => "N",
+		"OFFERS_PROPS" => array("COLOR_REF"),
+		"PATH_TO_ORDER" => "/personal/order/make/",
+		"PRICE_VAT_SHOW_VALUE" => "Y",
+		"QUANTITY_FLOAT" => "N",
+		"SET_TITLE" => "Y",
+		"TEMPLATE_THEME" => "site",
+		"USE_GIFTS" => "Y",
+		"USE_PREPAYMENT" => "N"
+	)
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
