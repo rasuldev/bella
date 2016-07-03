59caab75c0e6a4ec3964272559eb8516eafbbdfc
Rustam <m-rustam1996@rambler.ru>
2016-07-03 14:50:14 +0300

    some fix;

--- login/novaya-stranitsa.php
+++ login/novaya-stranitsa.php
@@ -1,21 +1,17 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница");
-?><?$APPLICATION->IncludeComponent("bitrix:main.register","my_reg_template",Array(
-				"AUTH" => "Y",
-				"REQUIRED_FIELDS" => array(
-						'EMAIL',
-						'PASSWORD',
-						'CONFIRM_PASSWORD'),
-				"SET_TITLE" => "Y",
-				"SHOW_FIELDS" => array(
-						'EMAIL',
-						'PASSWORD',
-						'CONFIRM_PASSWORD'),
-				"SUCCESS_PAGE" => "",
-				"USER_PROPERTY" => array(),
-				"USER_PROPERTY_NAME" => "",
-				"USE_BACKURL" => "Y"
-		)
-);?>
-	<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.register",
+	"",
+	Array(
+		"AUTH" => "Y",
+		"REQUIRED_FIELDS" => array("EMAIL","TITLE","NAME","SECOND_NAME","LAST_NAME","PERSONAL_PHONE"),
+		"SET_TITLE" => "Y",
+		"SHOW_FIELDS" => array("EMAIL","TITLE","NAME","SECOND_NAME","LAST_NAME","WORK_PHONE"),
+		"SUCCESS_PAGE" => "",
+		"USER_PROPERTY" => array(),
+		"USER_PROPERTY_NAME" => "",
+		"USE_BACKURL" => "Y"
+	)
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
