0cf48f875a85bba94adf3263fd7e4cc942ea548e
Rustam <m-rustam1996@rambler.ru>
2016-07-05 16:30:09 +0300

    some fix

--- catalog/index.php
+++ catalog/index.php
@@ -107,13 +107,62 @@ $APPLICATION->SetTitle("Каталог");
 
                 <aside class="have_banner">
                     <div class="sidebar_banner">
-                        <a href="javascript:void(0)">
-                            <img src="<?=SITE_TEMPLATE_PATH?>/img/sidebar_banner.jpg" alt=""/>
-                            <p>
-                                Новая <br /><span>коллекция</span>
-                            </p>
-                            <div class="skidka">скидка 50%</div>
-                        </a>
+						<?$APPLICATION->IncludeComponent(
+								"bitrix:news.list",
+								"slychainaya_akciya",
+								Array(
+										"ACTIVE_DATE_FORMAT" => "d.m.Y",
+										"ADD_SECTIONS_CHAIN" => "Y",
+										"AJAX_MODE" => "N",
+										"AJAX_OPTION_ADDITIONAL" => "",
+										"AJAX_OPTION_HISTORY" => "N",
+										"AJAX_OPTION_JUMP" => "N",
+										"AJAX_OPTION_STYLE" => "Y",
+										"CACHE_FILTER" => "N",
+										"CACHE_GROUPS" => "Y",
+										"CACHE_TIME" => "36000000",
+										"CACHE_TYPE" => "A",
+										"CHECK_DATES" => "Y",
+										"DETAIL_URL" => "",
+										"DISPLAY_BOTTOM_PAGER" => "Y",
+										"DISPLAY_DATE" => "Y",
+										"DISPLAY_NAME" => "Y",
+										"DISPLAY_PICTURE" => "Y",
+										"DISPLAY_PREVIEW_TEXT" => "Y",
+										"DISPLAY_TOP_PAGER" => "N",
+										"FIELD_CODE" => array("ID","DETAIL_TEXT","DETAIL_PICTURE",""),
+										"FILTER_NAME" => "",
+										"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+										"IBLOCK_ID" => "6",
+										"IBLOCK_TYPE" => "offers",
+										"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+										"INCLUDE_SUBSECTIONS" => "Y",
+										"MESSAGE_404" => "",
+										"NEWS_COUNT" => "20",
+										"PAGER_BASE_LINK_ENABLE" => "N",
+										"PAGER_DESC_NUMBERING" => "N",
+										"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+										"PAGER_SHOW_ALL" => "N",
+										"PAGER_SHOW_ALWAYS" => "N",
+										"PAGER_TEMPLATE" => ".default",
+										"PAGER_TITLE" => "Новости",
+										"PARENT_SECTION" => "",
+										"PARENT_SECTION_CODE" => "",
+										"PREVIEW_TRUNCATE_LEN" => "",
+										"PROPERTY_CODE" => array("akcii_skidka",""),
+										"SET_BROWSER_TITLE" => "Y",
+										"SET_LAST_MODIFIED" => "N",
+										"SET_META_DESCRIPTION" => "Y",
+										"SET_META_KEYWORDS" => "Y",
+										"SET_STATUS_404" => "N",
+										"SET_TITLE" => "Y",
+										"SHOW_404" => "N",
+										"SORT_BY1" => "ACTIVE_FROM",
+										"SORT_BY2" => "SORT",
+										"SORT_ORDER1" => "DESC",
+										"SORT_ORDER2" => "ASC"
+								)
+						);?>
                     </div>
                 </aside>
 
@@ -165,8 +214,8 @@ $APPLICATION->SetTitle("Каталог");
         );
         ?>
         <?$APPLICATION->IncludeComponent(
-	"bitrix:catalog.section", 
-	"catalog", 
+	"bitrix:catalog.section",
+	"catalog",
 	array(
 		"IBLOCK_TYPE_ID" => "catalog",
 		"IBLOCK_ID" => "2",
@@ -288,7 +337,7 @@ $APPLICATION->SetTitle("Каталог");
 	false
 );?>
                 </div><!--prod_list-->
-                <!-- 
+                <!--
                 <div class="pagi_on clear">
                     <a href="" class="prev_page"><i class="ico_left"></i></a>
                     <a href="" class="next_page"><i class="ico_right"></i></a>
