56b38e363b9846b8a57eda43e5bfd3f1baf94959
Rustam <m-rustam1996@rambler.ru>
2016-07-05 14:25:51 +0300

    complete Akcii

--- bitrix/templates/.default/components/bitrix/news.list/akcii_template/template.php
+++ bitrix/templates/.default/components/bitrix/news.list/akcii_template/template.php
@@ -16,38 +16,33 @@ $this->setFrameMode(true);
 
 $arSelect = array("ID", "PROPERTY_akcii_element", "IBLOCK_SECTION_ID");
 $arFilter = Array("IBLOCK_ID" => 6);
-$akcii_res = CIBlockElement::GetList(Array(), $arFilter, false, array(), $arSelect);
+$akcii_res = CIBlockElement::GetList(Array("SORT" => "ASC"), $arFilter, false, array(), $arSelect);
 while ($arItem = $akcii_res->GetNext()) {
     $product = CIBlockElement::GetByID($arItem["PROPERTY_AKCII_ELEMENT_VALUE"]);
     if ($arProduct = $product->Fetch()) {
         $section = CIBlockSection::GetByID($arProduct["IBLOCK_SECTION_ID"]);
       if ($arSection = $section->Fetch()) {
-          $arElement[$arItem["ID"]]["SECTION_CODE"] = $arSection["CODE"];
-          $arElement[$arItem["ID"]]["NAME_CODE"] = $arProduct["CODE"];
+          $arElement[$arItem["ID"]]["URL"] = "http://".$_SERVER["SERVER_NAME"]."/catalog/".$arSection["CODE"]."/".$arProduct["CODE"];
       }
     }
 }
-
-var_dump($arElement);
 ?>
 
-<div class="inner_page clear">
     <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
         <?= $arResult["NAV_STRING"] ?><br/>
     <? endif; ?>
     <div class="akcii clear">
-        <!--		$arResult["ITEMS"] as $arItem-->
         <div>
             <p>
                 <a class="wow bounceInLeft" style="visibility: visible; animation-name: bounceInLeft;"
-                   href="catalog/<?=$arElement[$arResult["ITEMS"][0]["ID"]]["SECTION_CODE"].'/'.$arElement[$arResult["ITEMS"][0]["ID"]]["NAME_CODE"]?>/">
+                   href="<?=$arElement[$arResult["ITEMS"][0]["ID"]]["URL"]?>">
                     <img src="<?= $arResult["ITEMS"][0]["DETAIL_PICTURE"]["SRC"] ?>" alt="">
                 </a>
             </p>
 
             <p>
                 <a class="wow bounceInRight" style="visibility: visible; animation-name: bounceInRight;"
-                   href="catalog/<?=$arElement[$arResult["ITEMS"][1]["ID"]]["SECTION_CODE"].'/'.$arElement[$arResult["ITEMS"][1]["ID"]]["NAME_CODE"]?>/">
+                   href="<?=$arElement[$arResult["ITEMS"][1]["ID"]]["URL"]?>">
                     <img src="<?= $arResult["ITEMS"][1]["DETAIL_PICTURE"]["SRC"] ?>" alt="">
                 </a>
             </p>
@@ -56,7 +51,7 @@ var_dump($arElement);
         <div>
             <p>
                 <a class="wow bounceIn" style="visibility: visible; animation-name: bounceIn;"
-                   href="catalog/<?=$arElement[$arResult["ITEMS"][2]["ID"]]["SECTION_CODE"].'/'.$arElement[$arResult["ITEMS"][2]["ID"]]["NAME_CODE"]?>/">
+                   href="<?=$arElement[$arResult["ITEMS"][2]["ID"]]["URL"]?>">
                     <img src="<?= $arResult["ITEMS"][2]["DETAIL_PICTURE"]["SRC"] ?>" alt="">
                 </a>
             </p>
@@ -65,14 +60,14 @@ var_dump($arElement);
         <div>
             <p>
                 <a class="wow flipInY" style="visibility: visible; animation-name: flipInY;"
-                   href="catalog/<?=$arElement[$arResult["ITEMS"][3]["ID"]]["SECTION_CODE"].'/'.$arElement[$arResult["ITEMS"][3]["ID"]]["NAME_CODE"]?>/">
+                   href="<?=$arElement[$arResult["ITEMS"][3]["ID"]]["URL"]?>">
                     <img src="<?= $arResult["ITEMS"][3]["DETAIL_PICTURE"]["SRC"] ?>" alt="">
                 </a>
             </p>
 
             <p>
                 <a class="wow flipInY" style="visibility: visible; animation-name: flipInY;"
-                   href="catalog/<?=$arElement[$arResult["ITEMS"][4]["ID"]]["SECTION_CODE"].'/'.$arElement[$arResult["ITEMS"][4]["ID"]]["NAME_CODE"]?>/">
+                   href="<?=$arElement[$arResult["ITEMS"][4]["ID"]]["URL"]?>">
                     <img src="<?= $arResult["ITEMS"][4]["DETAIL_PICTURE"]["SRC"] ?>" alt="">
                 </a>
             </p>
@@ -81,7 +76,7 @@ var_dump($arElement);
         <div>
             <p>
                 <a class="wow pulse" style="visibility: visible; animation-name: pulse;"
-                   href="catalog/<?=$arElement[$arResult["ITEMS"][5]["ID"]]["SECTION_CODE"].'/'.$arElement[$arResult["ITEMS"][5]["ID"]]["NAME_CODE"]?>/">
+                   href="<?=$arElement[$arResult["ITEMS"][5]["ID"]]["URL"]?>">
                     <img src="<?= $arResult["ITEMS"][5]["DETAIL_PICTURE"]["SRC"] ?>" alt="">
                 </a>
             </p>
@@ -89,4 +84,3 @@ var_dump($arElement);
 
 
     </div>
-</div>
--- events/index.php
+++ events/index.php
@@ -1,45 +1,86 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Акции");
-?>
-<div id="main_container">
+?><div id="main_container">
+	<div class="inner_page clear">
+		<div class="row">
+			<div class="col-lg-12" id="navigation">
+				 <?$APPLICATION->IncludeComponent(
+	"bitrix:breadcrumb",
+	"",
+	Array(
+		"PATH" => "",
+		"SITE_ID" => "-",
+		"START_FROM" => "0"
+	),
+false,
+Array(
+	'HIDE_ICONS' => 'Y'
+)
+);?>
+<h1 class="bx-title dbg_title" id="pagetitle"><?=$APPLICATION->ShowTitle(false);?></h1>
+			</div>
+		</div>
 
-    <div class="inner_page clear">
-        <div class="row">
-            <div class="col-lg-12" id="navigation">
-                <?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "", array(
-                        "START_FROM" => "0",
-                        "PATH" => "",
-                        "SITE_ID" => "-"
-                    ),
-                    false,
-                    Array('HIDE_ICONS' => 'Y')
-                );?>
-            </div>
-        </div>
-        <h1 class="bx-title dbg_title" id="pagetitle"><?=$APPLICATION->ShowTitle(false);?></h1>
-
-        <div class="akcii clear">
-
-            <div>
-                <p><a class="wow bounceInLeft" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/ban1.jpg" alt=""/></a></p>
-                <p><a class="wow bounceInRight" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/ban2.jpg" alt=""/></a></p>
-            </div>
-            <div>
-                <p><a class="wow bounceIn" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/ban3.jpg" alt=""/></a></p>
-            </div>
-            <div>
-                <p><a class="wow flipInY" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/ban4.jpg" alt=""/></a></p>
-                <p><a class="wow flipInY" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/ban5.jpg" alt=""/></a></p>
-            </div>
-            <div>
-                <p><a class="wow pulse" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/ban6.jpg" alt=""/></a></p>
-            </div>
-
-        </div><!--akcii-->
-
-    </div><!--inner_page-->
-
-
-</div> <!--#content_main-->
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+					 <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"akcii_template",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array("ID","DETAIL_PICTURE",""),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "6",
+		"IBLOCK_TYPE" => "offers",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array("",""),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ID",
+		"SORT_BY2" => "ID",
+		"SORT_ORDER1" => "ASC",
+		"SORT_ORDER2" => "ASC"
+	)
+);?>
+			 <!--akcii-->
+		</div>
+	</div>
+	 <!--inner_page-->
+</div>
+ <!--#content_main--><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
