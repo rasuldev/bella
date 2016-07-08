31eeec3d9d71ac6bb9f2e4ae77a8745f36d10aa5
Salikh Magomedov <saarus.painter@gmail.com>
2016-07-08 12:19:22 +0300

    Subcription updateв

--- index.php
+++ index.php
@@ -192,11 +192,10 @@ $APPLICATION->SetTitle("Bellashop");
  </li>
 		</ul>
 	</div>
-</div>-->
- <!--slider-->&nbsp;<?$APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"main_slider", 
-	array(
+</div>--> <!--slider-->&nbsp;<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"main_slider",
+	Array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "Y",
 		"AJAX_MODE" => "N",
@@ -209,6 +208,7 @@ $APPLICATION->SetTitle("Bellashop");
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "main_slider",
 		"DETAIL_URL" => "",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
@@ -216,10 +216,7 @@ $APPLICATION->SetTitle("Bellashop");
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
+		"FIELD_CODE" => array(0=>"",1=>"",),
 		"FILTER_NAME" => "",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"IBLOCK_ID" => "4",
@@ -238,11 +235,7 @@ $APPLICATION->SetTitle("Bellashop");
 		"PARENT_SECTION" => "",
 		"PARENT_SECTION_CODE" => "",
 		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "TITLE",
-			1 => "TEXT",
-			2 => "",
-		),
+		"PROPERTY_CODE" => array(0=>"TITLE",1=>"TEXT",2=>"",),
 		"SET_BROWSER_TITLE" => "Y",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_META_DESCRIPTION" => "Y",
@@ -253,10 +246,8 @@ $APPLICATION->SetTitle("Bellashop");
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"COMPONENT_TEMPLATE" => "main_slider"
-	),
-	false
+		"SORT_ORDER2" => "ASC"
+	)
 );?><br>
 <div id="main_container">
 	<div class="banner_block clear">
@@ -619,11 +610,28 @@ $APPLICATION->SetTitle("Bellashop");
 		<div>
 			<h2 class="wow zoomIn">подпишитесь на нашу <br>
 			 рассылку</h2>
-			<form action="#">
+			<!--<form action="#">
 				<p class="clear wow zoomIn">
  <input type="text" placeholder="Введите ваш Е-Mail"> <input type="submit" value="подписаться">
 				</p>
-			</form>
+			</form>-->
+			<?$APPLICATION->IncludeComponent(
+				"bitrix:sender.subscribe",
+				"index_subscribe",
+				Array(
+					"AJAX_MODE" => "N",
+					"AJAX_OPTION_ADDITIONAL" => "",
+					"AJAX_OPTION_HISTORY" => "N",
+					"AJAX_OPTION_JUMP" => "N",
+					"AJAX_OPTION_STYLE" => "Y",
+					"CACHE_TIME" => "3600",
+					"CACHE_TYPE" => "A",
+					"CONFIRMATION" => "Y",
+					"SET_TITLE" => "Y",
+					"SHOW_HIDDEN" => "N",
+					"USE_PERSONALIZATION" => "Y"
+				)
+			);?>
 		</div>
 	</div>
 	 <!--podpiska--> <!--<div class="our_brends">
